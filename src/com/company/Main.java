b3777d80b2db5f4dd8bcf973eecbe7e34a1cc681
lida <kurogiri.dabi@gmail.com>
2022-04-02 12:03:53 +0300

    init commit2

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите n ");
+        int n = scanner.nextInt();
+        System.out.print("Введите x ");
+        int x = scanner.nextInt();
+        double result=0;
+        final double p = 3.14;
+        for (int j = 1; j <= n; j++) {
+
+            double summa = p/2 + (Math.pow((-1), (n+1))/(2*n+1) * Math.pow(x, 2*n+1));
+            result =result + summa;
+        }
+        System.out.println(result);
+    }
+}
